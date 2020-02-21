fce0bbd8d87c808abccecd5472e2e99f6699eeb6
Genaro <gdimartino@devkingos.com>
2020-02-20 21:35:05 -0300

    RestServer -usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,20 @@
+//Puerto
 process.env.PORT = process.env.PORT || 3000;
+
+
+// Entorno
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+// BDD
+let urlDB;
+
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = 'mongodb://localhost:27017/cafe'
+// } else {
+//     urlDB = 'mongodb://gdimartino:sHTkmRBzAs1aELbp@cluster0-jh8tc.mongodb.net/cafe';
+// }
+
+urlDB = 'mongodb+srv://gdimartino:sHTkmRBzAs1aELbp@cluster0-jh8tc.mongodb.net/cafe';
+
+process.env.URLDB = urlDB;
