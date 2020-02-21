efba98d123380950a4086927cb171e0726f8d362
Genaro <gdimartino@devkingos.com>
2020-02-21 13:28:22 -0300

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -5,6 +5,12 @@ process.env.PORT = process.env.PORT || 3000;
 // Entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// Vencimiento Token
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// Seed autenticacion
+process.env.SEED = process.env.SEED || 'seed-desarrollo';
+
 
 // BDD
 let urlDB;
@@ -13,11 +19,6 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
     urlDB = 'mongodb+srv://genaro:rPqrca6hjmnQcOhE@cluster0-ucbb3.mongodb.net/cafe';
-    // urlDB = 'mongodb://genaro:rPqrca6hjmnQcOhE@cluster0-ucbb3.mongodb.net/cafe';
 }
 
 process.env.URLDB = urlDB;
-
-
-//mongoose.connect('mongodb+srv://genaro:rPqrca6hjmnQcOhE@cluster0-ucbb3.mongodb.net/cafe', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, resp) => {
-//mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, resp) => {
--- server/server.js
+++ server/server.js
@@ -7,11 +7,15 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
+// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
+// parse application/json
 app.use(bodyParser.json())
 
-app.use(require('./routes/usuario'));
+//Configuracion global de rutas
+app.use(require('./routes/index'));
+
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err, resp) => {
     if (err) throw err;
